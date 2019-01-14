a55723515a8e41d858e4b35a8d996c30b0f11941
Anni-Laptop <blue.cat@t-online.de>
2019-01-14 18:15:36 +0100

    Upload with simple form
    - more than one item possible
    - selection of more than one with strg (usability!)

---
+++ MusicAnalyzer/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class FileForm (forms.Form):
+    files = forms.FileField(
+         widget=forms.ClearableFileInput(attrs={'multiple': True}), label="Files", required=False)
--- MusicAnalyzer/views.py
+++ MusicAnalyzer/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+from MusicAnalyzer.forms import FileForm
 
 
 class Choice(View):
+    form_class= FileForm
+    template_name = "MusicAnalyzer/Choice.html"
+    context_dict = {"heading": "Choose here"}
+
     def get(self, request):
-        context_dict = {"heading": "Choose here"}
+        file_form = self.form_class()
+        self.context_dict["file_form"] = file_form
         # information from the context dictionary can be referenced in the template
-        return render(request, "MusicAnalyzer/Choice.html", context_dict)
+        return render(request, "MusicAnalyzer/Choice.html", self.context_dict)
 
     # handle data getting back from view
     def post(self, request):
-        pass
+        file_form = self.form_class(request.POST, request.FILES)
+        files = request.FILES.getlist('files')
+        if file_form.is_valid():
+            for f in files:
+                print(f.name)
+            self.context_dict.update({"message": "form is valid", "file_form": file_form})
+            return render(request, "MusicAnalyzer/Choice.html", self.context_dict)
+        else:
+            self.context_dict.update({"message": "form not valid", "file_form": file_form})
+            return render(request, "MusicAnalyzer/Choice.html", self.context_dict)
