584e1bc59fbdad0dc0cb330858fd9e83c85181d9
Anni-Laptop <blue.cat@t-online.de>
2019-01-19 16:22:33 +0100

    define search form

--- MusicAnalyzer/forms.py
+++ MusicAnalyzer/forms.py
@@ -3,3 +3,10 @@ from django import forms
 
 class FileForm (forms.Form):
     files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label="Files", required=False)
+
+
+class SearchForm (forms.Form):
+    composer = forms.CharField(max_length=150, required=False)
+    title = forms.CharField(max_length=200, required=False)
+    start_year = forms.IntegerField(max_value=9999, min_value=0, required=False)
+    end_year = forms.IntegerField(max_value=9999, min_value=0, required=False)
