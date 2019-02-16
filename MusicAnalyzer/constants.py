c00ef0618ce5030f07421a076bd91755747932ea
Anni-Laptop <blue.cat@t-online.de>
2019-02-16 15:28:35 +0100

    save selected music in cookie

--- MusicAnalyzer/constants.py
+++ MusicAnalyzer/constants.py
@@ -7,3 +7,7 @@ CORPUS_FILE = 1
 STATE_SEARCH_CORPUS = "search_corpus"
 STATE_UPLOAD_FILE = "upload"
 STATE_SELECT_FOR_ANALYSIS = "select_for_analysis"
+
+
+# Cookie names
+CHOSEN_MUSIC_COOKIE = "chosen_music"
--- MusicAnalyzer/session_handling.py
+++ MusicAnalyzer/session_handling.py
@@ -1,5 +1,7 @@
 from django.core import management
 
+from MusicAnalyzer import constants
+
 
 def on_session_start(request):
     # make sure that all expired sessions are cleaned out of db
@@ -7,3 +9,14 @@ def on_session_start(request):
     # 0 means that session is expired on browser closing
     s = request.session
     s.set_expiry(0)
+
+
+# persists music choices for the duration of a session
+def save_music_choice_to_cookie(request, music_pieces):
+    request.session[constants.CHOSEN_MUSIC_COOKIE] = music_pieces
+
+
+def access_music_choice_from_cookie(request):
+    if request.session.get(constants.CHOSEN_MUSIC_COOKIE):
+        print(request.session[constants.CHOSEN_MUSIC_COOKIE])
+        return request.session[constants.CHOSEN_MUSIC_COOKIE]
