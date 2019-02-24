b95c55440cfed6620cfad57b3095b1fcc8923cd6
Anni-Laptop <blue.cat@t-online.de>
2019-02-24 23:05:00 +0100

    view sessions (test whether sessions are deleted)

--- MusicAnalyzer/models.py
+++ MusicAnalyzer/models.py
@@ -1,3 +1,20 @@
+import os
+import shutil
+
+from django.contrib.sessions.models import Session
 from django.db import models
 
 # Create your models here. (models = equivalent of db tables)
+from django.db.models.signals import pre_delete
+
+from DH_201819_MusicAnalysis import settings
+
+
+def session_end_handler(sender, **kwargs):
+    session_folder = kwargs.get('instance').session_key
+    path = settings.MEDIA_ROOT
+    shutil.rmtree(os.path.join(path, session_folder))
+    print("session %s ended" % kwargs.get('instance').session_key)
+
+
+pre_delete.connect(session_end_handler, sender=Session)
--- MusicAnalyzer/session_handling.py
+++ MusicAnalyzer/session_handling.py
@@ -1,9 +1,19 @@
+from django.contrib.sessions.models import Session
 from django.core import management
 
 
 def on_session_start(request):
     # make sure that all expired sessions are cleaned out of db
     management.call_command('clearsessions')
+    print("num active sessions: ")
+    print(Session.objects.count())
+    sessions = Session.objects.all()
+    for session in sessions:
+        print(session.session_key)
+        print(session.expire_date)
+
     # 0 means that session is expired on browser closing
     s = request.session
+    print("new session ")
     s.set_expiry(0)
+
