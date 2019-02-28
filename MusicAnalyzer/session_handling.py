9663585d98e76863cf1be0049c9efec58638babf
Anni-Laptop <blue.cat@t-online.de>
2019-02-28 20:05:33 +0100

    bugfix: session_key is now available right after session starts

--- MusicAnalyzer/session_handling.py
+++ MusicAnalyzer/session_handling.py
@@ -10,7 +10,11 @@ def on_session_start(request):
     # make sure that all expired sessions are cleaned out of db
     management.call_command('clearsessions')
     # 0 means that session is expired on browser closing
-    s = request.session
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+        s = request.session
+    else:
+        s = request.session
     s.set_expiry(0)
     new_path = os.path.join(settings.MEDIA_ROOT, s.session_key)
     path_to_graphs = os.path.join(new_path, "graphs")
