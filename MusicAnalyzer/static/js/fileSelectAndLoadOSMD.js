248e65482fa66317fbc6e95c4166048302e0d38f
Maximiliane Windl <maxi.windl@webforuse.de>
2019-03-01 09:51:12 +0100

    add zoom controls

--- MusicAnalyzer/static/js/fileSelectAndLoadOSMD.js
+++ MusicAnalyzer/static/js/fileSelectAndLoadOSMD.js
@@ -1,12 +1,51 @@
-window.onload = function() {
-   triggerUpload()
+window.onload = function () {
+    triggerUpload()
 }
 
 function triggerUpload() {
+    let initialZoom = 0.6
+    let openSheetMusicDisplay = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas", {autoResize: true});
+    let canvas = document.getElementById("osmdCanvas")
     let localFile = document.getElementById("musicPiece").value;
-    let openSheetMusicDisplay = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmdCanvas");
+    let zoomIn = document.getElementById("zoom-in-btn");
+    let zoomOut = document.getElementById("zoom-out-btn");
+
     openSheetMusicDisplay.load(localFile)
-    openSheetMusicDisplay.zoom = 0.6;
+    openSheetMusicDisplay.zoom = initialZoom;
     openSheetMusicDisplay.render()
+
+    // Create zoom controls
+    zoomIn.onclick = function () {
+        console.log("click")
+        initialZoom *= 1.2;
+        scale();
+    };
+    zoomOut.onclick = function () {
+        initialZoom /= 1.2;
+        scale();
+    };
+
+    function scale() {
+        disable();
+        window.setTimeout(function () {
+            openSheetMusicDisplay.zoom = initialZoom;
+            openSheetMusicDisplay.render();
+            enable();
+        }, 0);
+    }
+
+
+    function disable() {
+        document.body.style.opacity = 0.3;
+        canvas.disabled = zoomIn.disabled = zoomOut.disabled = "disabled";
+    }
+
+    function enable() {
+        document.body.style.opacity = 1;
+        canvas.disabled = zoomIn.disabled = zoomOut.disabled = "";
+    }
+
 }
+
+
 
