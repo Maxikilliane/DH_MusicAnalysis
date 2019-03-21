c82ee767eaa46d9feb4aaf35f2143904ae003fed
Maximiliane Windl <maxi.windl@webforuse.de>
2019-03-21 14:58:22 +0100

    add chart for chord names

--- MusicAnalyzer/static/js/distant_analysis.js
+++ MusicAnalyzer/static/js/distant_analysis.js
@@ -2,7 +2,105 @@ function distantAnalysis(analysisJson) {
     console.log(analysisJson.per_piece_stats)
     createChordQualityCountChart(analysisJson)
     createChordRootCountChart(analysisJson)
+    createChordNameCountChart(analysisJson)
 
+}
+
+function createChordNameCountChart(analysisJson) {
+    // group stats by group
+    var grouped = _.mapValues(_.groupBy(analysisJson.per_piece_stats, 'group'),
+        clist => clist.map(car => _.omit(car, 'group')));
+
+
+    // group stats by chord root count and sum up values
+    var newGroup = []
+    for (let group in grouped) {
+        for (let arrayIndex in grouped[group]) {
+            newGroup[group] = sumObjectsByKey(newGroup[group], grouped[group][arrayIndex].chord_name_count)
+        }
+    }
+    for (groupName in newGroup) {
+        for (group in newGroup[groupName]) {
+            console.log(newGroup[groupName][group])
+            if (newGroup[groupName][group] < 5) {
+                console.log("im if")
+                console.log(newGroup[groupName][group])
+                delete newGroup[groupName][group]
+            }
+        }
+
+    }
+    console.log(newGroup)
+
+    // sum all group names in one array
+    let groupNames = Object.keys(newGroup)
+
+    let uniqueKeys = getUniqueKeys(newGroup)
+
+    let data = getMatchingVals(newGroup, uniqueKeys, groupNames)
+
+
+    // draw the chart
+    $(function () {
+        var options = {
+            seriesBarDistance: 10,
+            axisX:
+                {
+                    offset: 60
+                }
+            ,
+            axisY: {
+                offset: 80,
+                labelInterpolationFnc:
+
+                    function (value) {
+                        return value + ' CHF'
+                    }
+
+                ,
+                scaleMinSpace: 15
+            }
+        }
+
+        var responsiveOptions = [
+            ['screen and (max-width: 640px)', {
+                seriesBarDistance: 5,
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                        return value[0];
+                    }
+                }
+            }]
+        ];
+
+        new Chartist.Bar('.ct-chart-name', {
+                labels: uniqueKeys,
+                series: data,
+                responsiveOptions
+            }, {
+                plugins: [
+                    Chartist.plugins.legend({
+                        legendNames: groupNames,
+                    })
+                ]
+            },
+            {
+                seriesBarDistance: 10,
+                axisX: {
+                    offset: 60
+                },
+                axisY: {
+                    offset: 80,
+                    labelInterpolationFnc: function (value) {
+                        return value + ' CHF'
+                    },
+                    scaleMinSpace: 15
+                }
+
+            }
+        )
+        ;
+    });
 }
 
 function createChordRootCountChart(analysisJson) {
@@ -44,7 +142,7 @@ function createChordRootCountChart(analysisJson) {
             }]
         ];
 
-        new Chartist.Bar('.ct-chart', {
+        new Chartist.Bar('.ct-chart-root', {
             labels: uniqueKeys,
             series: data,
             options,
@@ -115,7 +213,7 @@ function createChordQualityCountChart(analysisJson) {
 }
 
 function getUniqueKeys(newGroup) {
-       // get only the keys of chord root count (e.g. A,C# etc.)
+    // get only the keys of chord root count (e.g. A,C# etc.)
     let allKeys = []
     for (let group in newGroup) {
         allKeys[group] = Object.keys(newGroup[group])
@@ -143,7 +241,7 @@ function getUniqueKeys(newGroup) {
 }
 
 function getMatchingVals(newGroup, uniqueKeys, groupNames) {
-        // map the groups by the keys from the unique array so that the values are in the right order
+    // map the groups by the keys from the unique array so that the values are in the right order
     let matchingVals = [];
     for (let chord in newGroup) {
         let toCheck = newGroup[chord]
