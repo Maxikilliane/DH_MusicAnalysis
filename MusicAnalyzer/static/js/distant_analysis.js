02876884fa694de82021b6312e9006dc299fabbe
Maximiliane Windl <maxi.windl@webforuse.de>
2019-03-23 12:37:32 +0100

    add boxplots to ambiuts analysis

--- MusicAnalyzer/static/js/distant_analysis.js
+++ MusicAnalyzer/static/js/distant_analysis.js
@@ -1,8 +1,9 @@
 function distantAnalysis(analysisJson) {
-    console.log(analysisJson.per_piece_stats)
+    console.log(analysisJson)
     createChordQualityCountChart(analysisJson)
     createChordRootCountChart(analysisJson)
     createChordNameCountChart(analysisJson)
+    drawBoxplots(analysisJson)
 
 }
 
@@ -21,16 +22,12 @@ function createChordNameCountChart(analysisJson) {
     }
     for (groupName in newGroup) {
         for (group in newGroup[groupName]) {
-            console.log(newGroup[groupName][group])
             if (newGroup[groupName][group] < 5) {
-                console.log("im if")
-                console.log(newGroup[groupName][group])
                 delete newGroup[groupName][group]
             }
         }
 
     }
-    console.log(newGroup)
 
     // sum all group names in one array
     let groupNames = Object.keys(newGroup)
@@ -265,4 +262,107 @@ function sumObjectsByKey(...objs) {
         }
         return a;
     }, {});
+}
+
+function drawBoxplots(analysisJson) {
+    var groupNames = []
+    var resultArray = []
+    for (let group in analysisJson.per_group_stats) {
+        if (analysisJson.per_group_stats[group].semitones_li != undefined) {
+            groupNames[group] = analysisJson.per_group_stats[group].group_name;
+            resultArray[group] = [analysisJson.per_group_stats[group].min_ambitus_semitones,
+                getPercentile(analysisJson.per_group_stats[group].semitones_li, 25),
+                analysisJson.per_group_stats[group].median_ambitus_semitones,
+                getPercentile(analysisJson.per_group_stats[group].semitones_li, 75),
+                analysisJson.per_group_stats[group].max_ambitus_semitones]
+        }
+    }
+    var myConfig = {
+        "graphset": [
+            {
+                "type": "boxplot",
+                "plotarea": {
+                    "margin": "100"
+                },
+                "scaleX": {
+                    "guide": {
+                        "visible": false
+                    },
+                    "label": {
+                        "text": "Group"
+                    },
+                    "values": groupNames,
+                    "item": {
+                        "wrapText": true
+                    }
+                },
+                "scaleY": {
+                    "minValue": "auto",
+                    "guide": {
+                        "lineStyle": "solid"
+                    },
+                    "label": {
+                        "text": "Number of semitones"
+                    },
+                    "item": {
+                        "wrapText": true
+                    }
+                },
+                "tooltip": {
+                    "fontSize": 11,
+                    "align": "left",
+                    "borderRadius": 7,
+                    "borderWidth": 1,
+                    "backgroundColor": "#fff",
+                    "alpha": 0.9,
+                    "padding": 10,
+                    "color": "#000"
+                },
+                "plot": {},
+                "options": {
+                    "box": {
+                        "barWidth": 0.5,
+                        "tooltip": {
+                            "text": "<span style=\"font-style:italic;\">Group %scale-key-text</span><br><b style=\"font-size:15px;color:%color3\">Number of semitones:</b><br><br>Maximum: <b>%data-max</b><br>Upper Quartile: <b>%data-upper-quartile</b><br>Median: <b>%data-median</b><br>Lower Quartile: <b>%data-lower-quartile</b><br>Minimum: <b>%data-min</b>"
+                        },
+                        "background-color": "#d70206",
+                        "border-color": "#d70206"
+                    },
+                    "outlier": {
+                        "tooltip": {
+                            "text": "<span style=\"font-style:italic;\">Group %scale-key-text</span><br><b style=\"font-size:15px;color:%color-7\">Number of semitones: %node-value</b>"
+                        },
+                        "marker": {
+                            "type": "circle"
+                        }
+                    }
+                },
+                "series": [
+                    {
+                        "dataBox": resultArray,
+                        "dataOutlier": []
+                    }
+                ]
+            }
+        ]
+    };
+
+    zingchart.render({
+        id: 'boxplots',
+        data: myConfig,
+        height: "100%",
+        width: "100%"
+    });
+}
+
+// we need Q1 and Q3 for the boxplots
+function getPercentile(data, percentile) {
+    var index = (percentile / 100) * data.length;
+    var result;
+    if (Math.floor(index) == index) {
+        result = (data[(index - 1)] + data[index]) / 2;
+    } else {
+        result = data[Math.floor(index)];
+    }
+    return result;
 }
