6fb58766b810ce0cccaa9cd08a803a91d055dadc
Anni-Laptop <blue.cat@t-online.de>
2019-03-27 22:55:14 +0100

    transfer to postgres db instead of sqlite

--- MusicAnalyzer/urls.py
+++ MusicAnalyzer/urls.py
@@ -5,13 +5,19 @@ from DH_201819_MusicAnalysis import settings
 app_name = 'MusicAnalyzer'
 
 # name nur bei Verwendung von Links aus anderen Seiten relevant
-urlpatterns = [
-    re_path(r'^individualChoice/$', IndividualChoice.as_view(), name="individual_choice"),
-    re_path(r'^distantChoice/$', DistantHearingChoice.as_view(), name="distant_choice"),
-    re_path(r'^index/$', Index.as_view(), name="index"),
-    re_path(r'^distantAnalysis/$', DistantAnalysis.as_view(), name="distant_analysis"),
-    re_path(r'^individualAnalysis/$', IndividualAnalysis.as_view(), name="individual_analysis")
-]
+try:
+    print("try")
+    urlpatterns = [
+        re_path(r'^individualChoice/$', IndividualChoice.as_view(), name="individual_choice"),
+        re_path(r'^distantChoice/$', DistantHearingChoice.as_view(), name="distant_choice"),
+        re_path(r'^index/$', Index.as_view(), name="index"),
+        re_path(r'^distantAnalysis/$', DistantAnalysis.as_view(), name="distant_analysis"),
+        re_path(r'^individualAnalysis/$', IndividualAnalysis.as_view(), name="individual_analysis")
+    ]
+
+except:
+    print("except")
+    urlpatterns = []
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
