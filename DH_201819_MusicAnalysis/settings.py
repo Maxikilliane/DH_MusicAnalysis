c9afff83b5c6eb4bf499764e0c94a7f0fbc07bfd
Maximiliane Windl <maxi.windl@webforuse.de>
2019-03-28 08:41:51 +0100

    just a test

--- DH_201819_MusicAnalysis/settings.py
+++ DH_201819_MusicAnalysis/settings.py
@@ -56,7 +56,10 @@ TEMPLATES = [
 SECRET_KEY = '8y3u15nm@((k$kllp2&%wg&^i7#7=@kc_6v+c)fu!u-y%=s91%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
