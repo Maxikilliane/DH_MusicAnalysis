a37925e9854ba8bc9ef6b880825a41bd844768d9
Anni-Laptop <blue.cat@t-online.de>
2019-04-07 11:38:00 +0200

    add download functionality for analysis result data

--- MusicAnalyzer/static/js/distant_analysis.js
+++ MusicAnalyzer/static/js/distant_analysis.js
@@ -16,6 +16,8 @@ $(document).ready(function () {
         type: "POST",
         dataType: 'json',
         success: function (json) {
+            var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json));
+            document.getElementById("download_json_button").href="data:"+data;
             distantAnalysis(json.all_summary_stats);
             addMetadata(json.metadata);
             UIkit.notification.closeAll();
